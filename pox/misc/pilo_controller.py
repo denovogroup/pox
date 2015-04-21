1cf1934bf2c52af29caae91b8ccc3cb9f747110c
Maxb <maxb.personal@gmail.com>
2015-04-20 23:41:53 -0700

    Migrating pilo_controller from twisted listener to OVS+pox packet in listener.

--- pox/misc/pilo_controller.py
+++ pox/misc/pilo_controller.py
@@ -24,9 +24,6 @@ from pox.misc.pilo_transport import PiloSender, PiloReceiver
 
 from pox.lib.recoco import Timer
 from pox.lib.addresses import IPAddr, IPAddr6, EthAddr
-from twisted.internet.protocol import DatagramProtocol
-from twisted.internet import reactor
-from threading import Thread
 from lib.util import get_hw_addr, get_ip_address
 import socket, struct
 import traceback
@@ -34,78 +31,13 @@ import json
 
 log = core.getLogger()
 
-class BroadcastHandler(DatagramProtocol):
-
-  def datagramReceived(self, data, (host, port)):
-    global controller
-    log.debug("received %r from %s:%d" % (data, host, port))
-
-    try:
-      broadcast_in = pkt.pilo.unpack(data)
-    except Exception as e:
-      log.debug(e)
-      log.debug('Can\'t parse as PILO packet')
-      return
-
-    log.debug('PILO Packet:')
-    log.debug(broadcast_in)
-
-    if pkt.packet_utils.same_mac(pkt.packet_utils.mac_string_to_addr(get_hw_addr(THIS_IF)), broadcast_in.src_address):
-      log.debug('This came from us, so we can ignore')
-      return
-
-    if broadcast_in.ACK:
-      if broadcast_in.SYN:
-        already_controlling = False
-        for controlled in controller.controlling:
-          if pkt.packet_utils.same_mac(controlled['mac'], broadcast_in.src_address):
-            already_controlling = True
-
-        if not already_controlling:
-          # This means that we've established a connection with the client
-          log.debug('Controlling: ' + str(broadcast_in.src_address))
-          controller.controlling.append({
-                'mac_to_port': {},
-                'mac': EthAddr(broadcast_in.src_address)
-              })
-          controller.sender.send_synack(broadcast_in)
-
-      controller.sender.handle_ack(broadcast_in)
-
-    else:
-      # This is an OVS PILO query - we *should* be able to handle it like a normal packet_in
-      log.debug(broadcast_in)
-
-      def handle_pilo_message(broadcast_in):
-        try:
-          inner_packet = of.ofp_packet_in()
-          log.debug('attempting to unpack payload from:')
-          log.debug(broadcast_in)
-          inner_packet.unpack(broadcast_in.payload)
-          log.debug('ofp_packet_in packet:')
-          # log.debug(inner_packet)
-          ethernet_packet = pkt.ethernet(inner_packet.data)
-          log.debug('ethernet packet:')
-          log.debug(ethernet_packet)
-
-          for client in controller.controlling:
-            if broadcast_in.src_address == client['mac']:
-              log.debug('This PILO packet matches our client:')
-              log.debug(client)
-              pilo_client = client
-
-              # TODO: This is where we would send back OF messages to switch
-
-        except Exception:
-          log.debug(traceback.format_exc())
-
-      controller.receiver.handle_packet_in(broadcast_in, handle_pilo_message)
-
 class PiloController:
   """
   A PiloController object will be created once at the startup of POX
   """
-  def __init__ (self, clients, sender, receiver):
+  def __init__ (self, connection, clients, sender, receiver):
+
+    self.connection = connection
     self.unacked = []
     self.controlling = []
     self.sender = sender
@@ -113,6 +45,40 @@ class PiloController:
 
     for client in clients:
       self.sender.send_syn(client)
+
+    # Creates an open flow rule which should send PILO broadcast messages
+    # to our handler
+    broadcast_msg_flow = of.ofp_flow_mod()
+    broadcast_msg_flow.priority = 100
+    broadcast_msg_flow.match.dl_type = pkt.ethernet.IP_TYPE
+    broadcast_msg_flow.match.nw_proto = pkt.ipv4.UDP_PROTOCOL
+    broadcast_msg_flow.match.nw_dst = IPAddr(UDP_IP) # TODO: better matching for broadcast IP
+    broadcast_msg_flow.actions.append(of.ofp_action_output(port = of.OFPP_CONTROLLER))
+
+    self.connection.send(broadcast_msg_flow)
+
+    normal_msg_flow = of.ofp_flow_mod()
+    normal_msg_flow.priority = 101
+    normal_msg_flow.match.dl_type = pkt.ethernet.IP_TYPE
+    normal_msg_flow.match.dl_src = pkt.packet_utils.mac_string_to_addr(get_hw_addr(THIS_IF))
+    normal_msg_flow.match.nw_proto = pkt.ipv4.UDP_PROTOCOL
+    normal_msg_flow.match.nw_dst = IPAddr(UDP_IP) # TODO: better matching for broadcast IP
+    normal_msg_flow.actions.append(of.ofp_action_output(port = of.OFPP_NORMAL))
+
+    self.connection.send(normal_msg_flow)
+
+    # A Final rule to send any ovs rule misses to the controller
+    # I believe that OF 1.0 does this automatically, but later versions do not
+    table_miss_msg_flow = of.ofp_flow_mod()
+    table_miss_msg_flow.priority = 1
+    table_miss_msg_flow.actions.append(of.ofp_action_output(port = of.OFPP_CONTROLLER))
+
+    self.connection.send(table_miss_msg_flow)
+
+
+    # This binds our PacketIn event listener
+    connection.addListeners(self)
+
 
   def send_control_msg(self, client, msg):
     # Takes an of message and sends it to client via PILO
@@ -126,6 +92,101 @@ class PiloController:
     log.debug(pilo_packet)
 
     self.sender.send(pilo_packet)
+
+
+  def _handle_PacketIn (self, event):
+    """
+    Handles packet in messages from the switch.
+    """
+
+    broadcast_in = event.parsed # This is the parsed packet data.
+
+    if not broadcast_in.parsed:
+      log.warning("Ignoring incomplete packet")
+      return
+
+    log.debug("handle packet in: ")
+    log.debug(broadcast_in)
+
+    eth = broadcast_in.find('ethernet')
+    local_mac = EthAddr(get_hw_addr(THIS_IF))
+
+    if pkt.packet_utils.same_mac(eth.src, local_mac):
+      log.debug('This is a packet from this switch!')
+      return
+
+    # Ignore ARP requests because the arp-responder module is doing this
+    a = broadcast_in.find('arp')
+    if a:
+      log.debug('This is an ARP request, so pilo_controller is gonna ignore it')
+      return
+
+    try:
+      udp = broadcast_in.find('udp')
+
+      pilo_packet = pkt.pilo(udp.payload)
+
+      log.debug('PILO packet: ' + str(pilo_packet))
+
+      dst_mac = EthAddr(pilo_packet.dst_address)
+
+      if pkt.packet_utils.same_mac(dst_mac, local_mac):
+
+        if pkt.packet_utils.same_mac(get_hw_addr(THIS_IF), pilo_packet.src_address):
+          log.debug('This came from us, so we can ignore')
+          return
+
+        if pilo_packet.ACK:
+          if pilo_packet.SYN:
+            already_controlling = False
+            for controlled in self.controlling:
+              if pkt.packet_utils.same_mac(controlled['mac'], pilo_packet.src_address):
+                already_controlling = True
+
+            if not already_controlling:
+              # This means that we've established a connection with the client
+              log.debug('Controlling: ' + str(pilo_packet.src_address))
+              self.controlling.append({
+                    'mac_to_port': {},
+                    'mac': EthAddr(pilo_packet.src_address)
+                  })
+              self.sender.send_synack(pilo_packet)
+
+          self.sender.handle_ack(pilo_packet)
+
+        else:
+          # This is an OVS PILO query - we *should* be able to handle it like a normal packet_in
+          log.debug(pilo_packet)
+
+          def handle_pilo_message(pilo_packet):
+            try:
+              inner_packet = of.ofp_packet_in()
+              log.debug('attempting to unpack payload from:')
+              log.debug(pilo_packet)
+              inner_packet.unpack(pilo_packet.payload)
+              log.debug('ofp_packet_in packet:')
+              # log.debug(inner_packet)
+              ethernet_packet = pkt.ethernet(inner_packet.data)
+              log.debug('ethernet packet:')
+              log.debug(ethernet_packet)
+
+              for client in self.controlling:
+                if pilo_packet.src_address == client['mac']:
+                  log.debug('This PILO packet matches our client:')
+                  log.debug(client)
+                  pilo_client = client
+
+                  # TODO: This is where we would send back OF messages to switch
+
+            except Exception:
+              log.debug(traceback.format_exc())
+
+          self.receiver.handle_packet_in(pilo_packet, handle_pilo_message)
+
+    except Exception as e:
+      log.debug(e)
+      log.debug('Can\'t parse as PILO packet')
+      return
 
 
 def launch (udp_ip, udp_port, this_if, tmp_dst_mac, retransmission_timeout="5"):
@@ -148,34 +209,18 @@ def launch (udp_ip, udp_port, this_if, tmp_dst_mac, retransmission_timeout="5"):
   src_ip = pkt.packet_utils.mac_string_to_addr(get_hw_addr(THIS_IF))
   src_address = get_hw_addr(THIS_IF)
 
-  def run ():
-    try:
-
-      sock = socket.socket(socket.AF_INET, # Internet
-          socket.SOCK_DGRAM) # UDP
-      sock.bind(('', UDP_PORT))
-      sock.setblocking(False)
-
-      port = reactor.adoptDatagramPort(
-          sock.fileno(), socket.AF_INET, BroadcastHandler(), maxPacketSize=65507)
-
-      sock.close()
-
-      log.debug("Listening on %s:%d" % (UDP_IP, UDP_PORT))
-
-      reactor.run(installSignalHandlers=0)
-
-    except Exception:
-      print traceback.format_exc()
-
   # TODO: Take this in from command line!
   clients = [TMP_DST_MAC]
 
   sender = PiloSender(UDP_IP, UDP_PORT, int(retransmission_timeout), src_address)
   receiver = PiloReceiver(src_ip, UDP_IP, UDP_PORT)
-  controller = PiloController(clients, sender, receiver)
 
-  thread = Thread(target=run)
-  thread.daemon = True
-  thread.start()
+  def start_switch (event):
+    sender = PiloSender(UDP_IP, UDP_PORT, int(retransmission_timeout), src_address)
+    receiver = PiloReceiver(src_ip, UDP_IP, UDP_PORT)
+
+    log.debug("Controlling %s" % (event.connection,))
+    PiloController(event.connection, clients, sender, receiver)
+
+  core.openflow.addListenerByName("ConnectionUp", start_switch)
 
